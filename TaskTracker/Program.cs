8a8df4c8e827b436e037b292bdefc6078b1cebd6
JefenaGracia <jefenagracia@gmail.com>
2025-04-17 09:46:34 -0500

    minor changes

--- TaskTracker/Program.cs
+++ TaskTracker/Program.cs
@@ -1,17 +1,25 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using TaskTracker.Data;
+using Microsoft.EntityFrameworkCore;
+using TaskTracker.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Load Google Maps API config
+builder.Services.Configure<GoogleMapsConfig>(
+    builder.Configuration.GetSection("GoogleMaps"));
+
+// Register services
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
+// Middleware pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
@@ -23,4 +31,5 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
 app.MapRazorPages();
+
 app.Run();
